17630cddea03924bdeff3791d4ed4222b9da24b3
Joyce Martinez <joycetezanos@gmail.com>
2021-04-09 13:12:32 -0500

    heroku deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn listopro.wsgi --log-file -
--- src/listopro/settings.py
+++ src/listopro/settings.py
@@ -121,10 +121,17 @@ USE_L10N = True
 
 USE_TZ = True
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(PROJECT_ROOT, "staticfiles")
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
